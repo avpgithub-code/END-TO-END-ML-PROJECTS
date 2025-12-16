5954d6acd5657deb5f620b480e1e24c8d6d88dde
Archit Pandya <architpandya@yahoo.com>
2025-12-16 13:14:43 -0500

    src setup.py package

---
+++ setup.py
@@ -0,0 +1,26 @@
+#-----------------------------------------------------
+# setup.py
+#-----------------------------------------------------
+from setuptools import setup, find_packages
+from typing import List
+
+HYPENATE = '-e .'
+#-----------------------------------------------------
+# Function to read requirements from a file and return requirements as a list
+#-----------------------------------------------------
+def get_requirements(file_path: str) -> list[str]:
+    with open(file_path, 'r') as file:
+        requirements = file.readlines()
+    return [req.strip() for req in requirements if req.strip() \
+            and not req.startswith('#') and req.strip() != HYPENATE]
+#-----------------------------------------------------
+# Package configuration
+#-----------------------------------------------------
+setup(
+    name="ml_project",
+    version="0.1",
+    author="Archit Pandya",
+    author_email="architpandya@yahoo.com",
+    packages=find_packages(),
+    install_requires=get_requirements("requirements.txt"),
+)
